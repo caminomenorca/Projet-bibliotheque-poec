8c9eafbec13e3f06075bdee8acc25677a7342f21
caminomenorca <89095646+caminomenorca@users.noreply.github.com>
2023-01-04 21:39:29 +0100

    problème affichage modal résolu

--- weBook.js
+++ weBook.js
@@ -1,9 +1,11 @@
 import { bookData } from '/bookData.js'
 
-const biblio = document.getElementById("bookshelf");
+const bookshelf = document.getElementById("bookshelf");
 const input = document.querySelector('input[type="file"]');
 const fileLabel = document.getElementById("fileLabel");
 const livreForm = document.getElementById("livreForm");
+const actualForm = document.getElementById("myForm");
+
 
 input.addEventListener('change', function () {
   const file = input.files[0];
@@ -24,61 +26,39 @@ input.addEventListener('change', function () {
 });
 console.log(bookData)
 
-function displayBooks(arr) {
-  console.log("displayBooks was called")
-  arr.forEach(function (obj) {
-    biblio.innerHTML += `
-    <div class="livreRayon">
-    <h3 class="titreLivre">${obj.titre}</h3>
-    <br>
-    <p class="auteurLivre">${obj.auteur} </p>
-    </div>`;})
-} 
-displayBooks(bookData)
-
-const actualForm = document.getElementById("myForm");
 livreForm.addEventListener("click", function () {
   console.log("Form was opened");
   actualForm.style.display = "block";
-});
-// Select the bookshelf element
-const bookshelf = document.querySelector('.bookshelf');
-
-// Function to create a new book element
-function createBookElement() {
-  const book = document.createElement('div');
-  book.classList.add('livreRayon');
-  book.textContent = 'Book';  // You can set the content of the book element here
-  return book;
-}
-
-// Function to check if the bookshelf is full
-function isBookshelfFull() {
-  // Get the width of the bookshelf and the width of a book element
-  const bookshelfWidth = bookshelf.offsetWidth;
-  const bookWidth = document.querySelector('livreRayon').offsetWidth;
-
-  // Calculate the maximum number of book elements that can fit in the bookshelf
-  const maxNumBooks = Math.floor(bookshelfWidth / bookWidth);
-
-  // Get the current number of book elements in the bookshelf
-  const numBooks = bookshelf.querySelectorAll('livreRayon').length;
-
-  // Return true if the number of book elements is equal to or greater than the maximum
-  return numBooks >= maxNumBooks;
-}
-
-// Function to fill the bookshelf with book elements
-function fillBookshelf() {
-  // Keep creating and appending new book elements until the bookshelf is full
-  while (!isBookshelfFull()) {
-    bookshelf.appendChild(createBookElement());
+}) 
+function displayBooks(arr) {
+  console.log("displayBooks was called")
+  
+  arr.forEach(function (obj) {
+    const livreRayon = document.createElement('div');
+    livreRayon.className = 'livreRayon';
+    
+    const titreLivre = document.createElement('h3');
+    titreLivre.className = 'titreLivre';
+    titreLivre.textContent = obj.titre;
+    
+    const auteurLivre = document.createElement('p');
+    auteurLivre.className = 'auteurLivre';
+    auteurLivre.textContent = obj.auteur;
+    
+    livreRayon.appendChild(titreLivre);
+    livreRayon.appendChild(document.createElement('br'));
+    livreRayon.appendChild(auteurLivre);
+    bookshelf.appendChild(livreRayon)
   }
-}
+    )
+    
+   
 
-// Call the fillBookshelf function to fill the bookshelf initially
-fillBookshelf();
+} 
 
+displayBooks(bookData);
+
+;
 
 
 
