f92af8285826b7bb27f80eb300a9f969fe668eec
Olivier Carle <ol.carle@gmail.com>
2023-01-06 15:51:14 +0100

    We hope we hop we gooooo

--- weBook.js
+++ weBook.js
@@ -24,31 +24,29 @@ const bookContainer =document.getElementById("bookContainer")
 
 /* Genere les livre dynamiquement */
 function displayBooks(arr) {
-  console.log("displayBooks was called")
-  
-  arr.forEach(function (obj) {
-    const livreRayon = document.createElement('div');
-    livreRayon.className = 'card';
-    
-    const titreLivre = document.createElement('h3');
-    titreLivre.className = 'titreLivre';
-    titreLivre.textContent = obj.titre;
-    
-    const auteurLivre = document.createElement('p');
-    auteurLivre.className = 'auteurLivre';
-    auteurLivre.textContent = obj.auteur;
-    
-    livreRayon.appendChild(titreLivre);
-    livreRayon.appendChild(document.createElement('br'));
-    livreRayon.appendChild(auteurLivre);
-    bookContainer.appendChild(livreRayon)
-  }
-    )
-    
-   
+	console.log("displayBooks was called");
 
+	arr.forEach(function (obj) {
+		const livreRayon = document.createElement("div");
+		livreRayon.className = "card hasOwnModal";
+		livreRayon.id = obj.titre;
+
+		const titreLivre = document.createElement("h3");
+		titreLivre.className = "titreLivre";
+		titreLivre.textContent = obj.titre;
+
+		const auteurLivre = document.createElement("p");
+		auteurLivre.className = "auteurLivre";
+		auteurLivre.textContent = obj.auteur;
+
+		livreRayon.appendChild(titreLivre);
+		livreRayon.appendChild(document.createElement("br"));
+		livreRayon.appendChild(auteurLivre);
+		bookContainer.appendChild(livreRayon);
+	});
 } 
-
 displayBooks(bookData);
+/* const hasOwnModal=document.getElementsByClassName("hasOwnModal");
+hasOwnModal.forEach(Elem=>document.getElementById(Elem.)) */
 
 ;
