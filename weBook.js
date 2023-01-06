c9d6c69af5424d17a36e8be451ff94ecff73cb49
Olivier Carle <ol.carle@gmail.com>
2023-01-06 12:29:12 +0100

    FUCK YEAH IT WORKS NOW (...kindoff)

--- weBook.js
+++ weBook.js
@@ -1,41 +1,34 @@
 import { bookData } from '/bookData.js'
 
-const bookshelf = document.getElementById("bookshelf");
 const input = document.querySelector('input[type="file"]');
-const fileLabel = document.getElementById("fileLabel");
-const livreForm = document.getElementById("livreForm");
 const actualForm = document.getElementById("myForm");
+const bookContainer =document.getElementById("bookContainer")
 
-
-input.addEventListener('change', function () {
+/* check if upload is an image */
+/* input.addEventListener('change', function () {
   const file = input.files[0];
   const fileReader = new FileReader();
   fileReader.onload = function () {
     try {
-      // create an image element with the data URI
+      
       const img = document.createElement('img');
       img.src = fileReader.result;
       img.className = "imagePetite"
       fileLabel.appendChild(img);
     } catch (e) {
-      // not an image
       console.error(e);
     }
   };
   fileReader.readAsDataURL(file);
-});
-console.log(bookData)
+}); */
 
-livreForm.addEventListener("click", function () {
-  console.log("Form was opened");
-  actualForm.style.display = "block";
-}) 
+/* Genere les livre dynamiquement */
 function displayBooks(arr) {
   console.log("displayBooks was called")
   
   arr.forEach(function (obj) {
     const livreRayon = document.createElement('div');
-    livreRayon.className = 'livreRayon';
+    livreRayon.className = 'card';
     
     const titreLivre = document.createElement('h3');
     titreLivre.className = 'titreLivre';
@@ -48,7 +41,7 @@ function displayBooks(arr) {
     livreRayon.appendChild(titreLivre);
     livreRayon.appendChild(document.createElement('br'));
     livreRayon.appendChild(auteurLivre);
-    bookshelf.appendChild(livreRayon)
+    bookContainer.appendChild(livreRayon)
   }
     )
     
